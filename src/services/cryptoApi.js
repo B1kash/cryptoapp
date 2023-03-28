e165f22acce581f1692e62fcb8a3813d37dd83f2
Bikash Baishya <bikash.baishya@borgos.in>
2023-03-28 08:56:41 +0530

    Line chart componet UI done

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,11 +16,17 @@ export const cryptoApi = createApi({
     endpoints: (builder) =>({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({coinId,refCurId, timePeriod}) => createRequest(`/coin/${coinId}/history?referenceCurrencyUuid=${refCurId}&timePeriod=${timePeriod}`),
         })
     })
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
 
 // const options = {
 //     method: 'GET',
